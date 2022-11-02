51b3ce02383bc7d06744d0bdb82d28e9d9b3b4bf
andreadldev <andrea.delisi2@gmail.com>
2022-11-02 17:54:10 +0100

    Correggi stile e aggiungi innerhtml ai quadrati

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ function createSquare() {
 }
 
 btn.addEventListener('click', function() {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 1; i <= 100; i++) {
         let element = createSquare();
         element.addEventListener('click', function () {
             this.classList.toggle('active');
         })
-    
+        element.innerHTML = i;
         grid.append(element);
     }
 }, {once: true});
